033c9143c83eaf199c97ac4b42e770bacd5df299
Dipankar Banerjee <banerjeedip761@gmail.com>
2025-07-31 18:50:44 +0530

    Commit done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import HomeAttack from "./pages/HomeAttack";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HomeAttack />
+    </div>
+  );
+}
+
+export default App;
